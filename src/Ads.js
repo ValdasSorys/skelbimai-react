1a6043ee0c937d619b1592470a303a34bae5e3d9
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2020-12-15 00:14:41 +0200

    Update Ads.js

--- src/Ads.js
+++ src/Ads.js
@@ -9,6 +9,15 @@ import {
 import { Pagination } from 'react-bootstrap';
 export class Ads extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = { matches: window.matchMedia("(min-width: 992px)").matches };
+    }
+    componentDidMount() {
+        const handler = e => this.setState({matches: e.matches});
+        window.matchMedia("(min-width: 992px)").addListener(handler);
+      }
     render()
   {
     let match = this.props.match;  
@@ -119,7 +128,8 @@ export class Ads extends React.Component
             <Ad id = {4} detailed = {false}/>
         </div>
         <div style={{"margin": "0 auto", "display": "table"}}>
-        <Pagination>
+        {this.state.matches &&
+        (<Pagination>
         <Pagination.First />
         <Pagination.Prev />
         <Pagination.Item>{1}</Pagination.Item>
@@ -135,7 +145,21 @@ export class Ads extends React.Component
         <Pagination.Item>{20}</Pagination.Item>
         <Pagination.Next />
         <Pagination.Last />
-        </Pagination>
+        </Pagination>)
+        }
+        {!this.state.matches &&
+        (<Pagination>
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+        <Pagination.Item>{11}</Pagination.Item>
+        <Pagination.Item active>{12}</Pagination.Item>
+        <Pagination.Item>{13}</Pagination.Item>
+        <Pagination.Ellipsis />
+        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Next />
+        </Pagination>)
+        }
         </div>
           </Route>
           <Route path="/">
