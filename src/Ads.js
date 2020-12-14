fe78286521d064d2ba537250c6a387e2114674a8
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2020-12-14 22:22:41 +0200

    asdsad

--- src/Ads.js
+++ src/Ads.js
@@ -6,22 +6,86 @@ import {
     Route,
     Redirect
   } from "react-router-dom";
+import { Pagination } from 'react-bootstrap';
 export class Ads extends React.Component
 {
     render()
   {
-    let match = this.props.match;        
+    let match = this.props.match;  
+    let active = 2;
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+    items.push(
+        <Pagination.Item key={number} active={number === active}>
+        {number}
+        </Pagination.Item>,
+    );
+}      
     return (      
-      <div>
+      <div className="elementContainer">
       <Switch>
           <Route exact path={`${match.path}/:id(\\d+)`} component={(props) => (<Ad detailed = {true} {...props}/>)}>
           </Route>
           <Route exact path="/ads">
             <h1>Skelbimai</h1>
+            <div className="filteringOptions">
+            <p>test</p>
+            <p>testtest</p>
+            </div>
+            <table className="entryElement">
+            <tr>
+                    <td className="entryFirst">
+                    First<br></br>
+                    Second
+                    </td>
+                    <td className="entrySecond">
+                    LastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLast
+                    </td>
+            </tr>
+            <tr>
+                    <td className="entryFirst">
+                    First<br></br>
+                    Second
+                    </td>
+                    <td className="entrySecond">
+                    LastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLast
+                    </td>
+            </tr>
+            <tr>
+                    <td className="entryFirst">
+                    First<br></br>
+                    Second
+                    </td>
+                    <td className="entrySecond">
+                    LastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLastLast
+                    </td>
+            </tr>
+        </table>
+        <div className="underFloat">
             <Ad id = {1} detailed = {false}/>
             <Ad id = {2} detailed = {false}/>
             <Ad id = {3} detailed = {false}/>
             <Ad id = {4} detailed = {false}/>
+        </div>
+        <div style={{"margin": "0 auto", "display": "table"}}>
+        <Pagination>
+        <Pagination.First />
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+
+        <Pagination.Item>{10}</Pagination.Item>
+        <Pagination.Item>{11}</Pagination.Item>
+        <Pagination.Item active>{12}</Pagination.Item>
+        <Pagination.Item>{13}</Pagination.Item>
+        <Pagination.Item>{14}</Pagination.Item>
+
+        <Pagination.Ellipsis />
+        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Next />
+        <Pagination.Last />
+        </Pagination>
+        </div>
           </Route>
           <Route path="/">
             <Redirect to="/404" />
--- src/index.js
+++ src/index.js
@@ -201,7 +201,7 @@ class Header extends React.Component {
               <Nav.Link onClick = {() => this.setActiveKey("/userslist")} eventKey = "/userslist" as = {Link} to="/userslist">Vartotojų sąrašas</Nav.Link>
               </Nav>
             navbarRight = 
-            <Nav activeKey={this.state.activeKey}>
+            <Nav className="ml-auto" activeKey={this.state.activeKey}>
               <Navbar.Text><i>Prisijungęs: {loginContext.user}</i></Navbar.Text>
               <Nav.Link onClick = {() => this.setActiveKey("/user")} eventKey = "/user" as = {Link} to={"/user/"+loginContext.id}>Profilis</Nav.Link>
               <Nav.Link onClick = {() => this.setActiveKey("/logout")} eventKey = "/logout" as = {Link} to="/logout">Atsijungti</Nav.Link>
@@ -215,7 +215,7 @@ class Header extends React.Component {
           <Nav.Link onClick = {() => this.setActiveKey("/ads")} eventKey = "/ads" as = {Link} to="/ads">Skelbimai</Nav.Link>     
           </Nav>
       navbarRight = 
-      <Nav activeKey={this.state.activeKey}>
+      <Nav className="ml-auto" activeKey={this.state.activeKey}>
           <Navbar.Text><i>Prisijungęs: {loginContext.user}</i></Navbar.Text>
           <Nav.Link onClick = {() => this.setActiveKey("/user")}  eventKey = "/user" as = {Link} to={"/user/"+loginContext.id}>Profilis</Nav.Link>
           <Nav.Link onClick = {() => this.setActiveKey("/logout")} eventKey = "/logout" as = {Link} to="/logout">Atsijungti</Nav.Link>
@@ -229,14 +229,14 @@ class Header extends React.Component {
               <Nav.Link onClick = {() => this.setActiveKey("/ads")} eventKey = "/ads" as = {Link} to="/ads">Skelbimai</Nav.Link>
               </Nav>
       navbarRight = 
-              <Nav activeKey={this.state.activeKey}>
+              <Nav className="ml-auto" activeKey={this.state.activeKey}>
               <Nav.Link onClick = {() => this.setActiveKey("/login")} eventKey = "/login" as = {Link} to="/login">Prisijungti</Nav.Link>
               </Nav>
     }
     return(
       <header>
       <Navbar sticky="top" collapseOnSelect expand="lg" bg="primary" variant="dark">
-      <Navbar.Brand>Skelbimų portalas</Navbar.Brand>
+      <Navbar.Brand>Skelbimai</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
       {navbarLeft}
