df1a22c94808f3b68f1d6a0ead9ce80a28761a7f
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2020-12-21 00:17:57 +0200

    updated url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://127.0.0.1:8000/api/"
+//export const API_URL = "http://127.0.0.1:8000/api/"
+export const API_URL = "https://skelbimai.azurewebsites.net/"
 export const TEST = "test"
 export const REGEXWORD = '[A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ,@.()?!/" "0-9-]+'
 export const REGEXUSERNAME = '[A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9]+'
@@ -7,4 +8,4 @@ export const REGEXNUMBER = '[0-9]+'
 //eslint-disable-next-line
 export const REGEXEMAILCHAR = '[A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9]+@[A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9]+\.[A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9]+'
 //eslint-disable-next-line
-export const REGEXPRICE = '[0-9]+(\.[0-9][0-9]?)?';
+export const REGEXPRICE = '[0-9]+(\.[0-9][0-9]?)?'
